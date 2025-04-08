02ce7b975b85293a22749d218cd5fa2eb696ff7d
Sergio Guirao Bernabé <151932115+sergioguirao0@users.noreply.github.com>
2025-04-08 22:01:11 +0200

    Refactorización y mejora del viewmodel

---
+++ BuildingProjectManagement/Resources/Strings/AppStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingProjectManagement.Resources.Strings
+{
+    public class AppStrings
+    {
+        // Rutas
+        public const string ApiBaseUrl = "https://localhost:7269/";
+        public const string RegisterEndpoint = "api/usuarios/registro";
+
+        public const string ApplicationJson = "application/json";
+
+        // Mensajes de error o confirmación
+        public const string RegisterSuccess = "Registro completado con éxito";
+        public const string RegisterFail = "Registro fallido";
+    }
+}
--- BuildingProjectManagement/ViewModel/UserViewModel.cs
+++ BuildingProjectManagement/ViewModel/UserViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using BuildingProjectManagement.Model;
+using BuildingProjectManagement.Resources.Strings;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -9,46 +11,81 @@ using System.Threading.Tasks;
 
 namespace BuildingProjectManagement.ViewModel
 {
-    public class UserViewModel
+    public class UserViewModel : ViewModelBase
     {
+        private string? _message;
+        public string? Message
+        {
+            get => _message;
+            set
+            {
+                if (_message != value)
+                {
+                    _message = value;
+                    OnPropertyChanged(nameof(Message));
+                }
+            }
+        }
+
         public async Task Register(User user)
         {
             try
             {
                 var client = new HttpClient();
-                client.BaseAddress = new Uri("https://localhost:7269/");
+                client.BaseAddress = new Uri(AppStrings.ApiBaseUrl);
 
                 var json = JsonSerializer.Serialize(user);
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var content = new StringContent(json, Encoding.UTF8, AppStrings.ApplicationJson);
 
-                var response = await client.PostAsync("api/usuarios/registro", content);
+                var response = await client.PostAsync(AppStrings.RegisterEndpoint, content);
 
                 if (response.IsSuccessStatusCode)
                 {
-                    Console.WriteLine("Registro completado con éxito");
+                    Message = AppStrings.RegisterSuccess;
                 }
                 else
                 {
-                    Console.WriteLine("Registro fallido");
+                    Message = AppStrings.RegisterFail;
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                Message = ex.Message;
             }
         }
 
-        public void ChangeFirstChar(string text)
+        private string ChangeFirstChar(string text)
         {
-            if (text.Contains(" "))
-            {
-                string[] words = text.Split(" ");
+            string resultado = "";
 
-                foreach (string word in words)
+            if (!string.IsNullOrEmpty(text))
+            {
+                if (text.Contains(" "))
                 {
-                    Console.WriteLine(word);
+                    string[] words = text.Split(" ");
+
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        resultado += char.ToUpper(words[i][0]) + words[i].Substring(1);
+
+                        if (i != words.Length - 1)
+                        {
+                            resultado += " ";
+                        }
+                    }
+                }
+                else
+                {
+                    resultado += char.ToUpper(text[0]) + text.Substring(1);
                 }
             }
+
+            return resultado;
+        }
+
+        private bool CheckDni(string dni)
+        {
+            return false;
         }
     }
 }
